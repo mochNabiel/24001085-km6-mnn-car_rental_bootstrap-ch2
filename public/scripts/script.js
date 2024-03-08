f398157f04714e62ca8eee61c970582543c36aa7
Mochammad nabiel <mochnabiel77@gmail.com>
2024-03-08 23:25:29 +0700

    fixing cars.html location

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const mulaiSewaButton = document.querySelector("#btn-sewa-mobil");
   if (mulaiSewaButton) {
     mulaiSewaButton.addEventListener("click", function () {
-      window.location.href = "/public/cars.html"; // Mengarahkan ke URL /cars
+      window.location.href = "/cars.html";
     });
   }
 });
